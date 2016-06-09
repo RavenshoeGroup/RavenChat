66d88e3ded8f674ea7d5b3e162af947b8b54829e
Beau Costoff <wbeaumont@rogers.com>
2016-06-09 09:00:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var numUsers = 0;
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/app/index.html');
+});
+
+
+io.on('connection', function(socket){
+
+	var addedUser = false;
+
+	socket.on('new message', function(data){
+		socket.broadcast.emit('new message', {
+			username: socket.username,
+			message: data
+		});
+	});
+
+	socket.on('add user', function(username){
+		if(addedUser) return;
+
+		socket.username = username;
+		++numUsers;
+		addedUser = true;
+		console.log(username + ' connected')
+
+		socket.emit('login', {
+	      numUsers: numUsers
+	    });
+		socket.broadcast.emit('user joined',{
+			username: socket.username,
+			numUsers: numUsers
+		});
+	});
+
+	socket.on('disconnect', function(){
+		if(addedUser){
+			--numUsers;
+
+			socket.broadcast.emit('user left', {
+				username: socket.username,
+				numUsers:numUsers
+			});
+		}
+	});
+	
+});
+
+
+http.listen(3000, function(){
+ 	console.log('listening on *:3000');
+});
